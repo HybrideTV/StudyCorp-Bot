edccb7f66b945be2f4bb3714fc81ec7fd2967f6f
HybrideTV <41163415+HybrideTV@users.noreply.github.com>
2023-02-23 14:10:53 +0100

    Delete src/utils/database/models directory

--- src/utils/database/models/mute.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const MuteModel = new mongoose.Schema({
-    userId: {type: String},
-	guildId: {type: String},
-	time: {type: Number}
-});
-
-const Mutes = module.exports = mongoose.model('mutes', MuteModel);
